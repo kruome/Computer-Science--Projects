6913b1c8beb25d837da0c73266f99641e88b817c
kruome <71830435+kruome@users.noreply.github.com>
2021-04-09 13:49:04 -0400

    Update Rectangle.h

--- CMPT102/Object Oriented Programming/Rectangle/Header Files: Rectangle/Rectangle.h
+++ CMPT102/Object Oriented Programming/Rectangle/Header Files: Rectangle/Rectangle.h
@@ -1,1 +1,11 @@
-
+class Rectangle{
+  private:
+  double width, height;
+  public:
+  double getWidth();
+  void setWidth(double);
+  double getHegiht();
+  void setHeight(double);
+  double getArea();
+  double getPerimeter();
+};
