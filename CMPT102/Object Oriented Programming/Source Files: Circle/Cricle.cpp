46fda9d0b065786b5f5c1c943408d07c0982bb1a
kruome <71830435+kruome@users.noreply.github.com>
2021-04-09 13:31:29 -0400

    Update Cricle.cpp

--- CMPT102/Object Oriented Programming/Source Files: Circle/Cricle.cpp
+++ CMPT102/Object Oriented Programming/Source Files: Circle/Cricle.cpp
@@ -1,1 +1,20 @@
+`cpp
+int main(){
+    
+    Circle c1, c2;
+    
+    c1.radius = 10;
+    cout<<"Radius: "<<c1.radius<<endl;
+    cout<<"Area: "<<c1.getArea()<<endl;
+    
+    cout<<"Enter the radius of the second circle: ";
+    cin>> c2.radius;
+    
+    cout<<"Radius: "<<c2.radius<<endl;
+    cout<<"Area: "<<c2.getArea()<<endl;
+    return 0;
+}
+double Circle::getArea() const{
+    return M_PI * pow(radius,2);
+}
 
