cf36d7bbbb49c9be075fc2583bda31f6951f0b3c
kruome <71830435+kruome@users.noreply.github.com>
2021-04-16 13:27:57 -0400

    Update Rectangle.cpp

--- CMPT102/Object Oriented Programming/Rectangle/Source Files: Rectangle/Rectangle.cpp
+++ CMPT102/Object Oriented Programming/Rectangle/Source Files: Rectangle/Rectangle.cpp
@@ -3,23 +3,26 @@
 double Rectangle::getWidth(){
   return width;
 }
-
 void Rectangle::setWidth(double width){
   this->width = width;
 }
-
 double Rectangle::getHeight(){
   return height;
 }
-
 void Rectangle::setHeight(double height){
   this->height = height;
 }
-
 double Rectangle::getArea(){
   return (width*height);
 }
-
 double Rectangle::getPerimeter(){
   return 2*(width + height);
+}
+Rectangle::Rectangle(){
+    height = 1;
+    width = 1;
+}
+Rectangle::Rectangle(double height, double width){
+    this->width = width;
+    this->height = height;
 }
