3ed2238d62e8a31b055590206be684248c661fe8
kruome <71830435+kruome@users.noreply.github.com>
2021-04-23 13:51:37 -0400

    Create student.cpp

---
+++ CMPT102/Object Oriented Programming/Person/Source Files: Person/student.cpp
@@ -0,0 +1,26 @@
+#incude "Student.h"
+
+Student::Student(string name, string phone, string email, int id, string major){
+  setName(name);
+  setPhone(phone);
+  setEmail(email);
+  this-> id = id;
+  this-> major = major;
+}
+int Student::getId(){
+  return id;
+}
+void Student::setId(int id){
+  this-> id = id;
+}
+string Student::getMajor(){
+  return major;
+}
+void Student::setMajor(string major){
+  this-> major = major;
+}
+void Student::printInfo(){
+  cout << "Name: " << getName() << endl;
+  cout << "Phone: " << getPhone() << endl;
+  cout << "Email: " << getEmail() << endl;
+}
