be94e2226af83fd64987e871dd0061090eeb1ddc
kruome <71830435+kruome@users.noreply.github.com>
2021-04-23 13:16:14 -0400

    Update person.h.cpp

--- CMPT102/Object Oriented Programming/Person/Header Files: Person/person.h.cpp
+++ CMPT102/Object Oriented Programming/Person/Header Files: Person/person.h.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+  private:
+    string name;
+    string phone;
+    string email;
+  public:
+    Person();
+    Person(string, string, string);
+    string getName();
+    void setName(string);
+    string getPhone();
+    void setPhone(string);
+    string getEmail();
+    void setEmail(string);
+    void printInfo();
+};
+    
