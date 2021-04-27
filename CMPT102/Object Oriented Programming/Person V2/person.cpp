4b2635bad2d16333c302a8bd43aee56b9c5cd9c8
kruome <71830435+kruome@users.noreply.github.com>
2021-04-27 13:28:26 -0400

    Create person.cpp

---
+++ CMPT102/Object Oriented Programming/Person V2/person.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+    public:
+    Person(){
+        cout<<"Person's constructor"<<endl;
+    }
+};
+
+class Employee : public Person{
+    public:
+    Employee(){
+        cout<<"Employee's constructor"<<endl;
+    }
+};
+
+int main(){
+    Employee e1;
+    return 0;
+}
