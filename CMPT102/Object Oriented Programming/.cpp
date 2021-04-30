d3c3b167c917925de70a2fb2837beb0e7a76cae4
kruome <71830435+kruome@users.noreply.github.com>
2021-04-30 13:29:38 -0400

    Update .cpp

--- CMPT102/Object Oriented Programming/.cpp
+++ CMPT102/Object Oriented Programming/.cpp
@@ -7,6 +7,24 @@ class Base{
     void y();
 };
 
+class Derived : public Base{
+  public: 
+    void y();
+};
+
 int main(){
+  Derived d;
+  d.x();
   return 0; 
+}
+
+void Base::x(){
+  y();
+}
+void Base::y(){
+  cout<<"Base class \n";
+}
+
+void Derived::y(){
+    cout<< "Derived class \n";
 }
