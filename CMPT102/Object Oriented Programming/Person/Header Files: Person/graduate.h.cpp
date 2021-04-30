f5816eb4e8667c33ba4b9d31fed32acb5ce82b4d
kruome <71830435+kruome@users.noreply.github.com>
2021-04-30 12:50:29 -0400

    Create graduate.h.cpp

---
+++ CMPT102/Object Oriented Programming/Person/Header Files: Person/graduate.h.cpp
@@ -0,0 +1,12 @@
+#include "Student.h"
+
+class Graduate {
+  private: 
+    string advisor;
+  public:
+    Graduate();
+    Graduate(string, string, string, int, string, string);
+	
+
+    void printInfo();
+};
