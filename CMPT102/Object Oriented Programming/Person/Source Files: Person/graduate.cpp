a5c65488f1b4ac601b54295cb90bfda56eddce11
kruome <71830435+kruome@users.noreply.github.com>
2021-04-30 13:13:37 -0400

    Create graduate.cpp

---
+++ CMPT102/Object Oriented Programming/Person/Source Files: Person/graduate.cpp
@@ -0,0 +1,18 @@
+#include "graduate.h"
+
+Graduate::Graduate(){
+  advisor = "unassigned";
+}
+Graduate::Graduate(string name, string phone, string email, int id, string major, string advisor) : Student(name, phone, email, id, major){
+  this->advisor = advisor;
+}
+String Graduate::getAdvisor(){
+  return advisor;
+}
+void Graduate::setAdvisor(){
+  this->advisor = advisor;
+}
+void Graduate::printInfo(){
+  Student::printInfo()
+  cout<<"Advisor: " << advisor <<endl;
+}
