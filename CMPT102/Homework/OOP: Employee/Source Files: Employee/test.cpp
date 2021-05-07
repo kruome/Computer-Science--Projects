ed448235ef48a1b6e74104fe6b5c7b2ac735e57a
kruome <71830435+kruome@users.noreply.github.com>
2021-05-07 00:29:43 -0400

    Create test.cpp

---
+++ CMPT102/Homework/OOP: Employee/Source Files: Employee/test.cpp
@@ -0,0 +1,10 @@
+int main(){
+    Staff s;
+    s.printInfo();
+    
+    cout<<endl;
+    
+    Faculty f("James", "304-291-2034", "jamesbrot@manhattan.edu", "Manhattan College", 20000, "Management", "Manager");
+    f.printInfo();
+    return 0;
+}
