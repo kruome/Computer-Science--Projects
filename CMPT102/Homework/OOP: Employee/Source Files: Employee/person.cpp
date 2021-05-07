4b32284369722711b6573e78210199990ec81a56
kruome <71830435+kruome@users.noreply.github.com>
2021-05-07 00:30:16 -0400

    Create person.cpp

---
+++ CMPT102/Homework/OOP: Employee/Source Files: Employee/person.cpp
@@ -0,0 +1,35 @@
+#include "Person.h"
+
+Person::Person(){
+  name = "example";
+  phone = "000-000-0000";
+  email = "example@manhattan.edu";
+}
+Person::Person(string name, string phone, string email){
+  this-> name = name;
+  this-> phone = phone;
+  this-> email = email;
+}
+string Person::getName(){
+  return name;
+}
+void Person::setName(string name){
+  this-> name = name;
+}
+string Person::getPhone( ){
+  return phone;
+}
+void Person::setPhone(string phone){
+  this-> phone = phone;
+}
+string Person::getEmail(){
+  return email;
+}
+void Person::setEmail(string email){
+  this->email = email;
+}
+void Person::printInfo(){
+  cout << "Name: " << name << endl;
+  cout << "Phone: " << phone << endl;
+  cout << "Email: " << email << endl;
+}
