a37cd1efc417d8945c0c7ded3f50ea94e87d1086
kruome <71830435+kruome@users.noreply.github.com>
2021-05-07 00:27:41 -0400

    Create staff.h.cpp

---
+++ CMPT102/Homework/OOP: Employee/Header Files: Employee/staff.h.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+class Staff : public Employee {
+    private:
+        string title;
+    public:
+        Staff(string, string, string, string, int, string);
+        Staff();
+        string getTitle();
+        void setTitle(string);
+        void printInfo();
+};
