77c125ac7e90601bef5cf722a09cefd8d1ec34b7
kruome <71830435+kruome@users.noreply.github.com>
2021-09-09 16:08:52 -0400

    Create Rounding.java

---
+++ CMPT360/Notes/Rounding.java
@@ -0,0 +1,14 @@
+package Initial_Examples;
+/**
+ * Upload pdf files for assignments on moodle
+    * Files should include the code and the snapshot of the output
+ * @author kdorji01
+ */
+
+public class notes {
+    public static void main(String[] args){
+        double a = 3.14159;
+        System.out.println(a);
+        System.out.printf("%.2f", a);
+    }
+}
