cc9cc1021e0889e6d57829bffd788ce2ff23fdbf
kruome <71830435+kruome@users.noreply.github.com>
2021-09-13 15:31:07 -0400

    Update Binary Search.cpp

--- CMPT238/Search Algorithms/Binary Search/Binary Search.cpp
+++ CMPT238/Search Algorithms/Binary Search/Binary Search.cpp
@@ -6,7 +6,7 @@ int binarySearch(int[], int, int);
 int main() {
 	
 	int B[6] = { 3,5,6,7,10,11 };
-	int	result = binarySearch(B, 6, 3);
+	int result = binarySearch(B, 6, 3);
 
 	if (result >= 0) {
 		cout << "A match found at location " << result << "\n";
