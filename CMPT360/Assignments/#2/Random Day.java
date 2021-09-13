e9bd5fcf42832484570f5a2ef14c0ae542c07931
kruome <71830435+kruome@users.noreply.github.com>
2021-09-13 14:51:29 -0400

    Create Random Day.java

---
+++ CMPT360/Assignments/#2/Random Day.java
@@ -0,0 +1,34 @@
+package selections;
+
+public class RandomDay {
+    public static void main(String[] args){
+        int number1 = 1+((int)(Math.random() * 7));
+        String day = "";
+    switch(number1){
+        case 1:
+            day = "Sunday";
+            break;
+        case 2:
+            day = "Monday";    
+            break;
+        case 3:
+            day = "Tuesday";
+            break;
+        case 4:
+            day = "Wednesday";
+            break;
+        case 5:
+            day = "Thursday";
+            break;
+        case 6:
+            day = "Friday";   
+            break;
+        case 7:
+            day = "Saturday";
+            break;
+    }
+                
+    System.out.println("The number generated was " + number1 + ". The day is " + day);
+    }
+   
+}
