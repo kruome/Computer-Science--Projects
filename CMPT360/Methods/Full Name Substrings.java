36b0e5fc8c401573c7032dd1e8f9d48694c7221d
kruome <71830435+kruome@users.noreply.github.com>
2021-09-20 14:46:57 -0400

    Create Full Name Substrings.java

---
+++ CMPT360/Methods/Full Name Substrings.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Compare {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter your first and last name: ");
+    String fname = input.nextLine();
+    String[] parts = (fname.split(" "));
+    System.out.println("Your first name is: " + parts[0] + " and your last name is: " + parts[1]);
+  }
+}
+-------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+public class Compare {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter your first and last name: ");    
+    String fname = input.nextLine();
+    int k = fname.indexOf(' ');
+    System.out.println("Your first name is: " + fname.substring(0, k) + " and your last name is:" + fname.substring(k));
+  }
+}
