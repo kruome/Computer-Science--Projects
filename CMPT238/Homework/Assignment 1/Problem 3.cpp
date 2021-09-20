11ad8313536da0748b0f03da9754ea4e59a934b2
kruome <71830435+kruome@users.noreply.github.com>
2021-09-20 13:06:23 -0400

    Create Problem 3.cpp

---
+++ CMPT238/Homework/Assignment 1/Problem 3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubblesort(char x[], int size);
+void print(char x[], int size);
+int main()
+{
+    char arr[10] = {'a','v','e','r','q','b','c','d','p','z'};
+    bubblesort(arr,10);
+    print(arr,10);
+    return 0;
+}
+void print(char x[], int size){
+    for(int i = 0; i < size;i++){
+        cout<<x[i] << " ";
+    }
+}
+void bubblesort(char x[], int size){
+    bool swapped = true;
+    int pass = 0;
+    for(int i = size - 1; i > 0 && swapped;i--){
+        swapped = false;
+        for(int j = 0; j < i; j++){
+            if(x[j] < x[j + 1]){
+                swap(x[j],x[j + 1]);
+                swapped = true;
+            }
+            pass++;
+            print(x,size);
+            cout<<"\n";
+        }
+    }
+    
+}
