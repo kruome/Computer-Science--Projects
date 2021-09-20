d835c19468eb5bf5d255780bb1b0a929d98223e1
kruome <71830435+kruome@users.noreply.github.com>
2021-09-20 07:30:41 -0400

    Update Linear Search with Class.cpp

--- CMPT238/Search Algorithms/Linear Search/Linear Search with Class.cpp
+++ CMPT238/Search Algorithms/Linear Search/Linear Search with Class.cpp
@@ -7,47 +7,28 @@ private:
 	string lname;
 	double gpa;
 public:
-	Student(string s1 = "X", string s2 = "Y", double d = 0) {
-		fname = s1; lname = s2; gpa = d;
-	}
-	string getfname() {
-		return fname;
-	}
-	string getlname() {
-		return lname;
-	}
-	double getgpa() { 
-		return gpa; 
-	}
+	Student(string s1 = "X", string s2 = "Y", double d = 0) { fname = s1; lname = s2; gpa = d; }
+	string getfname() { return fname; }
+	string getlname() { return lname; }
+	double getgpa() { return gpa; }
 
-	void setfname(string s) {
-		fname = s;
-	}
-	void setlname(string s) {
-		lname = s;
-	}
-	void setgpa(double d) {
-		gpa = d;
-	}
+	void setfname(string s) { fname = s; }
+	void setlname(string s) { lname = s; }
+	void setgpa(double d) { gpa = d; }
+
 };
-
-int LinearSearch(Student[], int, string);
-
-int main() {
-	Student arr[4] = {Student("A","S",3.0),Student("AW","RS",3.1) ,Student("FA","VS",3.5) ,Student("WZ","DZ",3.2) };
-	
-	bool result = LinearSearch(arr, 5, "FA");
-
-	if (result) {
-		cout << "A match found at location " << result << "\n";
-	}
-	else {
-		cout << "no match found\n";
-	}
-	
+int LinearSearch(Student list[], int size, double value);
+int main()
+{
+	Student Array1[4] = { Student("Alex","Smith",3.3),Student("John","Ramzi",2.4), Student("Ashly","David",3.6), Student("Suzan","Adam",2.9) };
+	int result;
+	result = LinearSearch(Array1, 4, 2.4);
+	if (result >= 0)
+		cout << " A match found at location " << result << "\n";
+	else cout << "No match found\n";
 }
 
-int LinearSearch(Student list[], int size, string value)
+int LinearSearch(Student list[], int size, double value)
 {
 	bool found = false;
 	int position = -1;
@@ -55,7 +36,7 @@ int LinearSearch(Student list[], int size, string value)
 
 	while (index < size && !found)
 	{
-		if (list[index].getfname() == value)
+		if (list[index].getgpa() == value)
 		{
 			found = true;
 			position = index;
