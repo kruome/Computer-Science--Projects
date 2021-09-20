f314d4628169089a0f8b63e88018d6d3f4a06319
kruome <71830435+kruome@users.noreply.github.com>
2021-09-20 10:36:26 -0400

    Update Binary Search with Class.cpp

--- CMPT238/Search Algorithms/Binary Search/Binary Search with Class.cpp
+++ CMPT238/Search Algorithms/Binary Search/Binary Search with Class.cpp
@@ -7,44 +7,51 @@ private:
 	string lname;
 	double gpa;
 public:
-	Student(string s1 = "X", string s2 = "Y", double d = 0) {
-		fname = s1; lname = s2; gpa = d;
-	}
-	string getfname() {
-		return fname;
-	}
-	string getlname() {
-		return lname;
-	}
-	double getgpa() { 
-		return gpa; 
-	}
+	Student(string s1 = "X", string s2 = "Y", double d = 0) { fname = s1; lname = s2; gpa = d; }
+	string getfname() { return fname; }
+	string getlname() { return lname; }
+	double getgpa() { return gpa; }
 
-	void setfname(string s) {
-		fname = s;
-	}
-	void setlname(string s) {
-		lname = s;
-	}
-	void setgpa(double d) {
-		gpa = d;
-	}
+	void setfname(string s) { fname = s; }
+	void setlname(string s) { lname = s; }
+	void setgpa(double d) { gpa = d; }
+
 };
+int LinearSearch(Student list[], int size, double value);
+int binarySearch(Student X[], int size, string value);
+int main()
+{
+	Student Array1[4] = { Student("Alex","Smith",3.3),Student("John","Ramzi",2.4), Student("Ashly","David",3.6), Student("Suzan","Adam",2.9) };
+	/*int result;
+	result = LinearSearch(Array1, 4, 2.4);
+	if (result >= 0)
+		cout << " A match found at location " << result << "\n";
+	else cout << "No match found\n";*/
 
-int binarySearch(Student[], int, string);
+	int result;
+	result =binarySearch(Array1, 4, "Adam");
+	if (result >= 0)
+		cout << " A match found at location " << result << "\n";
+	else cout << "No match found\n";
+}
 
-int main() {
-	Student arr[4] = {Student("Adam","dez",3.0),Student("Awaka","Ramzi",3.1) ,Student("Farans","Seint",3.5) ,Student("Waikz","Quiza",3.2) };
-	
-	int	result = binarySearch(arr, 4,"Ramzi");
+int LinearSearch(Student list[], int size, double value)
+{
+	bool found = false;
+	int position = -1;
+	int index = 0;
 
-	if (result) {
-		cout << "A match found at location " << result << "\n";
+	while (index < size && !found)
+	{
+		if (list[index].getgpa() == value)
+		{
+			found = true;
+			position = index;
+		}
+		++index;
+
 	}
-	else {
-		cout << "no match found\n";
-	}
-	
+	return position;
 }
 
 int binarySearch(Student X[], int size, string value)
@@ -63,7 +70,7 @@ int binarySearch(Student X[], int size, string value)
 			found = true;
 			position = middle;
 		}
-		else if (value > X[middle].getlname())
+		else if (value < X[middle].getlname())
 			first = middle + 1;
 		else
 			last = middle - 1;
