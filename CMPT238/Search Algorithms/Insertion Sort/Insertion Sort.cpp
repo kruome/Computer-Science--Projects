21db2e27063bc712fa141c77e0361d73cc987cd8
kruome <71830435+kruome@users.noreply.github.com>
2021-09-23 16:15:59 -0400

    Create Insertion Sort.cpp

---
+++ CMPT238/Search Algorithms/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>;
+using namespace std;
+
+void print(vector<double>&);
+
+void insertionSort(vector<double>& v);
+int main()
+{
+
+	vector<double> vec{ 10.0,-4,5,20,0.0,19 };
+
+	insertionSort(vec);
+	print(vec);
+
+
+}
+
+void printvec(vector<int>& v)
+{
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+	cout << "\n";
+
+}
+
+
+void insertionSort(vector<int>& v)
+{
+	int key;
+	int i;
+	for (int j = 1; j < v.size(); j++)
+	{
+		key = v[j];
+		i = j - 1;
+
+		while (i >= 0 && v[i] > key)
+		{
+			v[i + 1] = v[i];
+			i = i - 1;
+		}
+
+		v[i + 1] = key;
+	}
+
+
+}
