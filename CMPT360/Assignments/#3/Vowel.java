d9386a6109c189b73feb9171d789ba884c4f658c
kruome <71830435+kruome@users.noreply.github.com>
2021-09-25 18:53:08 -0400

    Update Vowel.java

--- CMPT360/Assignments/#3/Vowel.java
+++ CMPT360/Assignments/#3/Vowel.java
@@ -1,12 +1,50 @@
 import java.util.Scanner;
 public class letter{
     public static void main(String[] args){
-        System.out.println("Enter a letter (a-z): ");
+        System.out.print("Enter a letter (a-z): ");
         Scanner usrinput = new Scanner(System.in);
         String s = usrinput.next();
-        char v = Character.toLowerCase(s.charAt(0));
-        System.out.println("You entered: " + s );
+        char v = s.charAt(0);
 
+        if(Character.isLetter(v) == true){
+            switch(v) {
+            case 'a':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'A':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'e':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'E':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'i':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'I':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'o':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'O':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'u':
+                System.out.println(v + " is a vowel.");
+                break;
+            case 'U':
+                System.out.println(v + " is a vowel.");
+                break;
+            default:
+                System.out.println(v + " is not a vowel.");
+            }
+        }
+            else{
+                System.out.println(v + " is an invalid input");
+            }
         
     }
 }
