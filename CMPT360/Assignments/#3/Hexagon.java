249f76a105a301d76f105681b51386c47de93a32
kruome <71830435+kruome@users.noreply.github.com>
2021-09-26 12:42:51 -0400

    Update Hexagon.java

--- CMPT360/Assignments/#3/Hexagon.java
+++ CMPT360/Assignments/#3/Hexagon.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class telephone{
+public class Main{
     public static void main(String[] args){
         System.out.print("Enter the side: ");
         Scanner num = new Scanner(System.in);
