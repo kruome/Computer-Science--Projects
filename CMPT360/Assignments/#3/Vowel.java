ffd3f2d45dac73a1f5c8ffdf6c6457beb3cd0eee
kruome <71830435+kruome@users.noreply.github.com>
2021-09-26 12:38:33 -0400

    Update Vowel.java

--- CMPT360/Assignments/#3/Vowel.java
+++ CMPT360/Assignments/#3/Vowel.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class letter{
+public class Main{
     public static void main(String[] args){
         System.out.print("Enter a letter (a-z): ");
         Scanner usrinput = new Scanner(System.in);
