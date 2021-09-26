3cb3de866ff1f3b4c41760203cf8257bd8535a72
kruome <71830435+kruome@users.noreply.github.com>
2021-09-26 11:41:29 -0400

    Create Hexagon.java

---
+++ CMPT360/Assignments/#3/Hexagon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class telephone{
+    public static void main(String[] args){
+        System.out.print("Enter the side: ");
+        Scanner num = new Scanner(System.in);
+        String n = num.nextLine();
+        double area = 0;
+        if((Character.isDigit(n.charAt(0)) == true)){
+            double number = Double.parseDouble(n);
+            area = (6 * Math.pow(number,2)) / (4 * Math.tan(Math.PI/6));
+            System.out.println("The area of the hexagon is " + area);
+
+        }else{
+            System.out.println("Invalid input.");
+        }
+    }
+}
