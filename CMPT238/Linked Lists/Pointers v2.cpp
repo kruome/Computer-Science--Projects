2d1ba096fca68523f1eccda6ada90e5fb9c5ec32
kruome <71830435+kruome@users.noreply.github.com>
2021-10-07 16:06:10 -0400

    Update Pointers v2.cpp

--- CMPT238/Linked Lists/Pointers v2.cpp
+++ CMPT238/Linked Lists/Pointers v2.cpp
@@ -18,6 +18,8 @@ struct student {
 void printlist(student*);
 void add_node_end(student*&, int, string, double);
 void add_node_sorted(student*&, int, string, double);
+void deleteList(student* head);
+void deleteNode(int value, student*& head);
 int main() {
 
 	student* s1 = new student; // student* s1 = new students(2, "A", 3.5,)
@@ -33,15 +35,21 @@ int main() {
 
 	add_node_end(head, 2, "joe", 2.3);
 	head = new student(80, "Jak", 11, head);
+	deleteNode(2, head);
 
 	printlist(head);
+
 	cout << "____________\n\n";
 
 	student* head2 = nullptr;
 	add_node_end(head2, 100, "Thei", 3.99);
 	add_node_sorted(head2, 22, "TO", 4.5);
-	add_node_sorted(head2, 9, "L", 5);
-	printlist(head2);
+	add_node_sorted(head2, 21, "L", 5);
+    printlist(head2);
+    cout << " ___________\n\n";
+    deleteNode(24,head2);
+    printlist(head2);
+
 
 }
 void printlist(student* head) {
@@ -69,17 +77,54 @@ void add_node_end(student*& h, int i, string s, double d) {
 		temp->next = new student(i, s, d);
 	}
 }
-void add_node_sorted(student*& head, int i, string s, double d){
+void add_node_sorted(student*& head, int i, string s, double d) {
 	if (head == nullptr || i <= head->id) { //either the list is empty or 
-		head = new student(i,s,d,head); // the new id is smaller than the first
+		head = new student(i, s, d, head); // the new id is smaller than the first
 	}
 	else {
 		student* q = head;
 		student* prev = nullptr;
-		while (q!= nullptr || i > q->id) {
+		while (q != nullptr || i > q->id) {
 			prev = q;
 			q = q->next;
 		}
 		prev->next = new student(i, s, d, q);
 	}
 }
+void deleteList(student* head) {
+	student* garbage = head;
+	while (head) {
+		head = head->next;
+		delete garbage;
+		garbage = head;
+	}
+	cout << "The linked list is now empty. \n";
+}
+void deleteNode(int value, student*& head) {
+	if (head) {
+		student* p = head;
+		student* prev = nullptr;
+		bool found = false;
+		while (!found && p != nullptr) {
+			if (p->id == value) {
+				if (prev == nullptr) {
+					head = head->next;
+				}
+				else {
+					prev->next = p->next;
+					
+				p->next = nullptr;
+				
+				delete p;
+				found = true;
+				}
+			}
+			else {
+				prev = p;
+				p = p->next;
+			}
+		}
+	}
+}
+
+
