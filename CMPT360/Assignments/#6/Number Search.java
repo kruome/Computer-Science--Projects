9a0184e8e209095813c406acae2abdad8768811a
kruome <71830435+kruome@users.noreply.github.com>
2021-10-07 15:02:48 -0400

    Update Number Search.java

--- CMPT360/Assignments/#6/Number Search.java
+++ CMPT360/Assignments/#6/Number Search.java
@@ -1,8 +1,9 @@
-public class Main
+public class JavaApplication4
 {
-    public static int[] generateRandomArray(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = 1 + ((int)(Math.random() * 50));
+    public static int[] generateRandomArray(){
+        int arr[] = new int[20];
+        for(int i = 0; i < 20; i++){
+             arr[i]= 1 + ((int)(Math.random() * 50));
         }
         return arr;
     }
@@ -37,26 +38,48 @@ public class Main
             }
             return -1;
         }
+    
     public static void selectionSort(int arr[]){
-        int min = arr[0];
-        int minindex = 0;
-        int temp = 0;
-        
-        for (int i = 0; i < arr.length; i++){
-            min = arr[i];
-            minindex = i;
-            for ( int j = i + 1; j < arr.length; j++){
-                if(arr[j] < min){
-                    min = arr[j];
-                    minindex = j;
-                }
-                arr[i] = temp;
-                arr[i] = arr[minindex];
-                arr[minindex] = temp;
-            }
+        for (int i = 0; i < arr.length-1; i++){
+            int min = i; 
+            for (int j = i+1; j < arr.length; j++) 
+                if (arr[j] < arr[min]) 
+                    min = j; 
+              int temp = arr[min]; 
+            arr[min] = arr[i]; 
+            arr[i] = temp; 
         }
     }
+
+    
     public static void main(String[] args) {
+        int arr[] = generateRandomArray();
+        System.out.print("The randomly generated array of numbers is: ");
+        for(int i:arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
         
+        int randomkey = generateRandomKey() ;
+        System.out.println("The randomly generated key is: " + randomkey);          
+        if(linearSearch(arr,randomkey) == -1){
+            System.out.println("Using linear search, the key is not found in the original array.");
+        }else{
+            System.out.println("Using linear search, the key is found at index: " + linearSearch(arr,randomkey) + " within the sorted array.");
+        }
+        selectionSort(arr);
+        
+        System.out.print("The sorted array using selection sort is: "); 
+        for(int value:arr){
+            System.out.print(value +  " ");
+        }
+    
+        System.out.println();
+        
+        if(binarySearch(arr,randomkey) <= -1){
+            System.out.println("Using binary search, the key is not found in the sorted array but if it exited, it would be at position: " + -1*binarySearch(arr,randomkey));
+        }else{
+            System.out.println("Using binary search, the index of the key is found at index " + binarySearch(arr,randomkey) + " within the sorted array");
+        }
     }
 }
