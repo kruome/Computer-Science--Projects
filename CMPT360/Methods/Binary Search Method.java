4974b621cfb926d230d3145962264a12176498fa
kruome <71830435+kruome@users.noreply.github.com>
2021-10-07 15:05:27 -0400

    Create Binary Search Method.java

---
+++ CMPT360/Methods/Binary Search Method.java
@@ -0,0 +1,8 @@
+public class Untitled3{
+    public static void main(String[] args){
+        int[] list = {1,2,3,4,5,6,7,8,9,10};
+        System.out.print("Index is " + java.util.Arrays.binarySearch(list, 4));
+        int[] a ={33,12,98};
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
