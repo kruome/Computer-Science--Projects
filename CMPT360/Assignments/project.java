5c9f707de831fe33d02d7ef9eaead925396199c5
kruome <71830435+kruome@users.noreply.github.com>
2021-11-21 21:57:21 -0500

    Update project.java

--- CMPT360/Assignments/project.java
+++ CMPT360/Assignments/project.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.regex.*;
 import java.io.FileNotFoundException;
+
 
 
 
@@ -12,6 +14,30 @@ public class Main{
     Scanner instructorDatabase = new Scanner(instructorFile);
     File instructorFileSize = new File("instructor.txt");
     Scanner instructorDatabaseSize = new Scanner(instructorFileSize);
+    
+    File departmentFile = new File("department.txt");
+    Scanner departmentDatabase = new Scanner(departmentFile);
+    File departmentFileSize = new File("department.txt");
+    Scanner departmentDatabaseSize = new Scanner(departmentFile);
+    
+    int arrOfDeptSize = 0;
+    while(departmentDatabaseSize.hasNextLine()){
+        arrOfDeptSize++;
+        departmentDatabaseSize.nextLine();
+    }
+    
+    Department[] listOfDepartments = new Department[arrOfDeptSize];
+    for(int i = 0; i < listOfDepartments.length; i++){
+        listOfDepartments[i] = new Department();
+    }
+    for(int j = 0; j < listOfDepartments.length;j++){
+        String line = departmentDatabase.nextLine();
+        String[] deptField = line.split(",");
+        String departmentName = deptField[0];
+        String departmentLocation = deptField[1];
+        int departmentFund = Integer.parseInt(deptField[2].trim());
+        listOfDepartments[j] = new Department(departmentFund,departmentLocation,departmentName);
+    }
 
     //counts the number of items/lines in the file "instructor.txt" -- shows 6.
     int arrsize = 0;
@@ -20,26 +46,41 @@ public class Main{
       instructorDatabaseSize.nextLine();
     }
 
-    Scanner userInput = new Scanner(System.in);
-    int userOption = 0;
+
 
     Instructor[] listOfInstructors = new Instructor[arrsize];
-        for(int i = 0; i<listOfInstructors.length;i++){
-            listOfInstructors[i] = new Instructor();
-        }
-        for(int j = 0; j < listOfInstructors.length;j++){
-            String line = instructorDatabase.nextLine();
-            String[] fields = line.split(",");
-            int instructorID = Integer.parseInt(fields[0]);
-            String instructorName = fields[1];
-            //String instructorDepartment = fields[2];
-            listOfInstructors[j] = new Instructor(instructorID,instructorName);
-        }
+    for(int i = 0; i<listOfInstructors.length;i++){
+        listOfInstructors[i] = new Instructor();
+    }
 
+    for(int j = 0; j < listOfInstructors.length;j++){
+        String line = instructorDatabase.nextLine();
+        String[] instrucField = line.split(",");
+        int instructorID = Integer.parseInt(instrucField[0].trim());
+        String instructorName = instrucField[1];
+        String instructorDept = instrucField[2];
+        listOfInstructors[j] = new Instructor(instructorID,instructorName,instructorDept);
+        String instructorLocation = "";
+        for(int a = 0; a < listOfDepartments.length; a++){
+            if(listOfInstructors[j].getDept().equals(listOfDepartments[a].getDept())){
+                instructorLocation = listOfDepartments[a].getLocation();
+            }
+        }
+        listOfInstructors[j] = new Instructor(instructorID, instructorName, instructorDept, instructorLocation);
+    }
+    
+    Scanner userInput = new Scanner(System.in);
+    int userOption = 0;
+    
     //menu which has 3 options and stops when option 3 is entered -- work in progress    
     int option1LoopCount = 0;
     Scanner option1 = new Scanner(System.in);
     int option1UserInput = 0;
+    
+    // option2 scanners
+    Scanner option2Name = new Scanner(System.in);
+    Scanner option2ID = new Scanner(System.in);
+    Scanner option2Dept = new Scanner(System.in);
     while(userOption != 3){
         System.out.println("        Menu: ");
         System.out.println("1. Get instructor information");
@@ -50,13 +91,14 @@ public class Main{
         switch(userOption){
             case 1:
                 System.out.println();
-                System.out.println("Enter the instructor ID: ");
+                System.out.print("Enter the instructor ID: ");
                 option1UserInput = option1.nextInt();
                 for(int i = 0; i < listOfInstructors.length;i++){
                     if(listOfInstructors[i].getID() == option1UserInput){
                         System.out.println();
                         System.out.println("Instructor name: " + listOfInstructors[i].getName());
-                        //System.out.println("Instructor department: " + listOfInstructors[i].getDepartment());
+                        System.out.println("Instructor department: " + listOfInstructors[i].getDept());
+                        System.out.println("Instructor department location: " + listOfInstructors[i].getLocation());
                         option1LoopCount++;
                     }
                 }
@@ -68,14 +110,58 @@ public class Main{
                 System.out.println();
                 break;
             case 2:
+                
                 System.out.println();
-                System.out.println("2 got chosen");
-                System.out.println();
+                System.out.print("Enter the instructor ID: ");
+                String option2IDUserInput = option2ID.nextLine();
+                boolean option2IDFlag = false;
+                boolean continueFlag = true;
+                if(option2IDUserInput.matches("\\d{4}")){
+                    option2IDFlag = true;
+                }
+                while(option2IDFlag == false){
+                    System.out.print("Please enter a valid ID: ");
+                    option2IDUserInput = option2ID.nextLine();
+                    if(option2IDUserInput.matches("\\d{4}")){
+                        option2IDFlag = true;
+                    }
+                }
+                for(int i = 0; i < listOfInstructors.length; i++){
+                    if(listOfInstructors[i].getID() == Integer.parseInt(option2IDUserInput)){
+                        System.out.println("Instructor ID already exists in the file.");
+                        System.out.println();
+                        continueFlag = false;
+                        break;
+                    }
+                }
+                if(continueFlag == false){
+                    break;
+                }else{
+                    int newInstructorID = Integer.parseInt(option2IDUserInput);
+                
+                    System.out.print("Enter the instructor name: ");
+                    String option2NameUserInput = option2Name.nextLine();
+                    boolean option2NameFlag = false;
+                    if(option2NameUserInput.matches("^[A-Za-z]+")){
+                        option2NameFlag = true;
+                    }
+                    while(option2NameFlag = false){
+                        System.out.println("Please enter a valid name: ");
+                        option2NameUserInput = option2Name.nextLine();
+                        if(option2NameUserInput.matches("\\[a-z]+\\s\\[a-z]+")){
+                            option2NameFlag = true;
+                        }
+                    }
+                }
                 break;
             case 3:
                 System.out.println();
                 System.out.println("Exited.");
                 System.out.println();
+                break;
+            default:
+                System.out.println();
+                System.out.println("Please enter a valid input.");
                 break;
         }
     }
@@ -118,17 +204,25 @@ public class Main{
     static class Instructor{
         private int ID;
         private String name;
-        private Department dept;
+        private String dept;
         private String location;
 
-        public Instructor(int ID, String name){
+        public Instructor(int ID, String name, String dept, String location){
             this.ID = ID;
             this.name = name;
+            this.dept = dept;
+            this.location = location;
+
+        }
+        public Instructor(int ID, String name, String dept){
+            this.ID = ID;
+            this.name = name;
+            this.dept = dept;
         }
         public Instructor(){
             ID = 0;
             name = "";
-            Department dept = new Department(0,"","");
+            dept = "";
             location = "";
         }
         public int getID(){
@@ -146,7 +240,11 @@ public class Main{
         public void setLocation(String location){
             this.location = location;
         }
-
+        public String getDept(){
+            return dept;
+        }
+        public void setDept(String dept){
+            this.dept = dept;
+        }
     }
-
 }
