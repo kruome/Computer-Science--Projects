deef8b27b8ab4d6940d78cea826fa4f16b7578e2
kruome <71830435+kruome@users.noreply.github.com>
2021-12-05 09:19:08 -0500

    Update project.cpp

--- CMPT238/project.cpp
+++ CMPT238/project.cpp
@@ -1,15 +1,33 @@
 
-1400,Tiffany Manwaring
-1399,Terence Stephenson
-1398,Horace Langstaff
-1397,Jed Hopkins
-1396,Gilbert Cobbett
-1395,Erin Robertson
-1394,Wynne Caldwell
-1393,Winona Burns
-1392,Virginia Shaw
-1391,Lovell Hampton
+usadb
 
+1371,Rita Oliver
+1372,Ferris Barrett
+1374,Jamie Cooke
+1376,Yvette Tillery
+1379,Horace Hayes
+1381,Alexander Thompson
+1382,Rusty Thornton
+1383,Grayson Kim
+1384,Bradley Ogley
+1385,Halsey Gregory
+1386,Homer Andrews
+1387,Ged Mallin
+1388,Sienna Mitchell
+1389,Jillian Binder
+1390,Vince Webb
+1391,Lovell Hampton
+1392,Virginia Shaw
+1393,Winona Burns
+1394,Wynne Caldwell
+1395,Erin Robertson
+1396,Gilbert Cobbett
+1397,Jed Hopkins
+1398,Horace Langstaff
+1399,Terence Stephenson
+1400,Tiffany Manwaring
+
+ausdb
 1400,Zachary Weaving
 1397,Leo Jacobs
 1396,Seth Arnold
@@ -20,18 +38,22 @@
 1375,Piper Lawson
 1374,Mila Glenn
 1389,Natalie Simpson
+1388,Nate Whelan
+1387,Jackson Kennedy
+1386,Jesse Griffin
+1385,Nicholas Coleman
+1383,Braxton Adams
+1381,Grace Coleman
+1380,Nathan Noble
+1379,Ava Weaver
+1378,Millie Evans
+1376,Zara Quin
+1375,Skye Baker
+1374,Amber Edwards
+1371,Lara Holt
+1370,Addison Magrath
 
-1400,Filippa Novikova
-1399,Ogrifina Kuzmina
-1398,Jekaterina Kozlova
-1397,Osip Mikhailov
-1396,Mariya Sokoloa
-1395,Alyosha Volkov
-1394,Nonna Koroleva
-1389,Artem Semenov
-1387,Iolanta Ivanova
-1386,Tasha Dmitrieva
-
+jpndb
 1400,Saito Kunio
 1398,Kono Yuichi
 1395,Noda Rei
@@ -42,7 +64,49 @@
 1387,Mochizuki Kameyo
 1386,Tamashiro Kaoru
 1384,Mayeda Sora
---------------------------------------------------------------------------------------------------------------------------------------------------------
+1383,Ikeda Ko
+1382,Yoshimura Makoto
+1381,Okubo Akio
+1380,Chino Suzu
+1379,Nagasawa Akihiro
+1378,Suko Eri
+1376,Izumi Saburo
+1370,Nakata Misaki
+1365,Nakao Nobu
+1364,Shinohara Masaru
+1362,Baba Rie
+1361,Okita Kotone
+1359,Imada Shig
+1358,Morioka Aika
+1357,Okumura Hiromi
+
+rusdb
+1400,Filippa Novikova
+1399,Ogrifina Kuzmina
+1398,Jekaterina Kozlova
+1397,Osip Mikhailov
+1396,Mariya Sokoloa
+1395,Alyosha Volkov
+1394,Nonna Koroleva
+1389,Artem Semenov
+1387,Iolanta Ivanova
+1386,Tasha Dmitrieva
+1385,Kalva Zhukova
+1384,Afonasei Tarasov
+1383,Yevgeniya Vasilieva
+1382,Demyan Frolov
+1381,Sashura Sokolova
+1380,Spiridon Volkov
+1379,Yegor Orlov
+1378,Nazariy Kiselev
+1375,Ioakim Frolov
+1374,Avdotia Sorokina
+1372,Varushka Fedoroa
+1371,Aleksandr Morozov
+1368,Leonid Orlov
+1367,Kuzma Smirnov
+1365,Samuil Lebedev
+//
 #include <bits/stdc++.h>
 #include <stdlib.h>
 #include <fstream>
@@ -84,349 +148,13 @@ public:
   void deleteRear ();
   int getFrontscore ();
   int getRearscore ();
-  string getFrontcountry ();
   string getFrontname ();
-  string getRearschool ();
-  string getRearcountry ();
-  string getRearname ();
-  int size ();
-  bool isEmpty ();
-  void erase ();
-  int printindex(int);
-
-};
-int Deque:: printindex(int x){
-    int val = -1;
-    Node* temp = new Node;
-    temp = front;
-    for(int i = 0; i < x; i ++){
-        temp=temp->next;
-    }
-    val = temp->score;
-    return val;
-}
-
-bool
-Deque::isEmpty ()
-{
-  return (front == NULL);
-}
-
-int
-Deque::size ()
-{
-  return Size;
-}
-
-void
-Deque::insertFront (int score, string name)
-{
-  Node *newNode = Node::getnode (score, name);
-  if (newNode == NULL)
-    cout << "OverFlow\n";
-  else
-    {
-      if (front == NULL)
-	rear = front = newNode;
-
-      else
-	{
-	  newNode->next = front;
-	  front->prev = newNode;
-	  front = newNode;
-	}
-
-      Size++;
-    }
-}
-
-void
-Deque::insertRear (int score, string name)
-{
-  Node *newNode = Node::getnode (score, name);
-  if (newNode == NULL)
-    cout << "OverFlow\n";
-  else
-    {
-      if (rear == NULL)
-	front = rear = newNode;
-
-      else
-	{
-	  newNode->prev = rear;
-	  rear->next = newNode;
-	  rear = newNode;
-	}
-
-      Size++;
-    }
-}
-
-void
-Deque::deleteFront ()
-{
-  if (isEmpty ())
-    cout << "UnderFlow\n";
-
-  else
-    {
-      Node *temp = front;
-      front = front->next;
-
-      if (front == NULL)
-	rear = NULL;
-      else
-	front->prev = NULL;
-      free (temp);
-
-      Size--;
-    }
-}
-
-void
-Deque::deleteRear ()
-{
-  if (isEmpty ())
-    cout << "UnderFlow\n";
-
-  else
-    {
-      Node *temp = rear;
-      rear = rear->prev;
-
-      if (rear == NULL)
-	front = NULL;
-      else
-	rear->next = NULL;
-      free(temp);
-
-      Size--;
-    }
-}
-
-int
-Deque::getFrontscore ()
-{
-  if (isEmpty ())
-    {
-      return -1;
-    }
-  else
-    {
-      return front->score;
-    }
-}
-
-string
-Deque::getFrontname ()
-{
-  if (isEmpty ())
-    {
-      return "Empty.";
-    }
-  else
-    {
-      return front->name;
-    }
-}
-
-int
-Deque::getRearscore ()
-{
-  if (isEmpty ())
-    {
-      return -1;
-    }
-  else
-    {
-      return rear->score;
-    }
-}
-
-string
-Deque::getRearname ()
-{
-  if (isEmpty ())
-    {
-      return "Empty.";
-    }
-  else
-    {
-      return rear->name;
-    }
-}
-
-void
-Deque::erase ()
-{
-  rear = NULL;
-  while (front != NULL)
-    {
-      Node *temp = front;
-      front = front->next;
-      free (temp);
-    }
-  Size = 0;
-}
-int interpolation_search (Deque p, int value, int size)
-{
-  int low = 0;
-  int high = size-1;
-  int mid = -1;
-  
-  int l = p.getFrontscore();
-  int h = p.getRearscore();
-  
- while (low <= high && value >= p.printindex(low) && value <= p.printindex(high))
-  {
-     if (low == high)
-     {
-         if(p.printindex(low)==value)
-         {
-             return low;
-         }else
-         {
-            return -1;
-         }
-    }else{
-    mid = low +  (((high - low) / (p.printindex(high) - p.printindex(low))) * (value - p.printindex(low)));
-    if (p.printindex(mid) < value)
-    {
-      low = mid+1;
-    }
-    else if (p.printindex(mid)> value)
-    {
-      high = mid-1;
-    }
-    else if(p.printindex(mid)==value)
-    {
-      return mid;
-    }
-   }
-  }
-  return -1;
-}
-
-int
-main ()
-{
-    std::ifstream myfile ("dataset.txt");
-
-    std::string myline;
-    if ( myfile.is_open() ) {
-        while ( myfile ) {
-        std::getline (myfile, myline);
-        std::cout << myline << ": " << myfile.tellg() << '\n';
-        }
-    }
-    
-  Deque scollection;
-  
-  scollection.insertRear (100, "A");
-  scollection.insertRear (200, "B");
-
-  cout<<"Welcome to the global scoreboard for the internatinal AXT test database!" << endl;
-  
-  int userinput; 
-  int countryinput;
-  int testscore;
-    while(userinput != 2){
-        cout<<"Please choose one of the one following options:" << endl;
-        cout<<"1.Database"<<endl;
-        cout<<"2.Exit"<<endl;
-        cin>>userinput;
-        cout<<endl;
-
-        if(userinput == 1){
-            cout<<"Please choose one of the following countries to find the test score data:"<<endl;
-            cout<<"1.USA"<<endl;
-            cout<<"2.Australia"<<endl;
-            cout<<"3.Japan"<<endl;
-            cout<<"4.China"<<endl;
-            cout<<"5.Russia"<<endl;
-            cin>>countryinput;
-            switch(countryinput){
-                case 1:
-                    cout<<"Please enter the test score you would like to search for (1300-1400): "<<endl;
-                    cin>>testscore;
-                    break;
-                case 2:
-                    cout<<"Please enter the test score you would like to search for (1300-1400): "<<endl;
-                    cin>>testscore;
-                    break;
-                case 3:
-                    cout<<"Please enter the test score you would like to search for (1300-1400):"<<endl;
-                    cin>>testscore;
-                    break;
-                case 4:
-                    cout<<"Please enter the test score you would like to search for (1300-1400):"<<endl;
-                    cin>>testscore;
-                    break;
-                case 5: 
-                    cout<<"Please enter the test score you would like to search for (1300-1400):"<<endl;
-                    cin>>testscore;
-                    break;
-                default:
-                    cout<<"Invalid input. Returning back to the main menu."<<endl;
-                    break;
-            }
-        }
-        if(userinput == 2){
-            cout<<"Exited." << endl;
-        }
-    }
-}
----------------------------------------------------------------------------------
-	#include <bits/stdc++.h>
-#include <stdlib.h>
-#include <fstream>
-#include<iostream>
-using namespace std;
-
-struct Node
-{
-  int score;
-  string name;
-  Node *prev, *next;
-
-  static Node *getnode (int score, string name)
-  {
-    Node *newNode = (Node *) malloc (sizeof (Node));
-      newNode->score = score;
-      newNode->name = name;
-      newNode->prev = newNode->next = NULL;
-      return newNode;
-  }
-};
-
-class Deque
-{
-  Node *front;
-  Node *rear;
-  int Size;
-
-public:
-    Deque ()
-  {
-    front = rear = NULL;
-    Size = 0;
-  }
-
-  void insertFront (int, string);
-  void insertRear (int, string);
-  void deleteFront ();
-  void deleteRear ();
-  int getFrontscore ();
-  int getRearscore ();
-  string getFrontcountry ();
-  string getFrontname ();
-  string getRearschool ();
-  string getRearcountry ();
   string getRearname ();
   int size ();
   bool isEmpty ();
   void erase ();
   int printindex (int);
+  string getName(int);
 
 };
 
@@ -444,6 +172,16 @@ Deque::printindex (int x)
   return val;
 }
 
+string Deque::getName(int x){
+    Node *temp = new Node;
+    temp = front;
+    string val;
+    for(int i = 0; i < x; i ++){
+        temp = temp->next;
+    }
+    val = temp->name;
+    return val;
+}
 bool Deque::isEmpty ()
 {
   return (front == NULL);
@@ -628,8 +366,6 @@ interpolation_search (Deque p, int value, int size)
 	      return -1;
 	    }
 	}
-      else
-	{
 	  mid =
 	    low +
 	    (((high - low) / (p.printindex (high) -
@@ -639,27 +375,26 @@ interpolation_search (Deque p, int value, int size)
 	    {
 	      low = mid + 1;
 	    }
-	  else if (p.printindex (mid) > value)
-	    {
+	  else {
 	      high = mid - 1;
 	    }
-	  else if (p.printindex (mid) == value)
+	  if (p.printindex (mid) == value)
 	    {
 	      return mid;
 	    }
 	}
-    }
-  return -1;
+	return -1;
 }
+
 
 int
 main ()
 {
-    
+   //Double ended queue data set structure for 4 countries 
   Deque usadataset;
   
   ifstream usadb;
-  usadb.open ("usa");
+  usadb.open ("usadb");
   string usafile;
   string usastrscore;
   string usaname;
@@ -673,9 +408,7 @@ main ()
 	  usaname = usafile.substr (5,usafilelength);
 	  int usaintscore = stoi(usastrscore);
 	  usadataset.insertRear(usaintscore, usaname);
-	  cout << usaintscore << " ";
-	  cout << usaname << '\n';
-	  if (usadb.eof())
+    	  if (usadb.eof())
 				break;
 	}
     }
@@ -687,7 +420,7 @@ main ()
   Deque ausdataset;
   
   ifstream ausdb;
-  ausdb.open ("australia");
+  ausdb.open ("ausdb");
   string ausfile;
   string ausstrscore;
   string ausname;
@@ -701,8 +434,6 @@ main ()
 	  ausname = ausfile.substr (5,ausfilelength);
 	  int ausintscore = stoi(ausstrscore);
 	  ausdataset.insertRear(ausintscore, ausname);
-	  cout << ausintscore << " ";
-	  cout << ausname << '\n';
 	  if (ausdb.eof())
 				break;
 	}
@@ -712,5 +443,94 @@ main ()
       cout << "Couldn't open file\n";
     }
     
-   
+    Deque jpndataset;
+    
+    ifstream jpndb;
+    jpndb.open("jpndb");
+    string jpnfile;
+    string jpnstrscore;
+    string jpnname;
+    if(jpndb.is_open()){
+        while(jpndb){
+            getline(jpndb,jpnfile);
+            int jpnfilelength = jpnfile.length();
+            jpnstrscore=jpnfile.substr(0,4);
+            jpnname =jpnfile.substr(5,jpnfilelength);
+            int jpnintscore = stoi(jpnstrscore);
+            jpndataset.insertRear(jpnintscore,jpnname);
+            if(jpndb.eof()){
+                break;
+            }
+        }
+    }
+    Deque rusdataset;
+    ifstream rusdb;
+    rusdb.open("rusdb");
+    string rusfile;
+    string russtrscore;
+    string rusname;
+    if(rusdb.is_open()){
+        while(rusdb){
+            getline(rusdb,rusfile);
+            int rusfilelength = rusfile.length();
+            russtrscore = rusfile.substr(0,4);
+            rusname = rusfile.substr(5,rusfilelength);
+            int rusintscore = stoi(russtrscore);
+            rusdataset.insertRear(rusintscore,rusname);
+            if(rusdb.eof()){
+                break;
+            }
+        }
+    }
+    
+    //Main Menu
+      cout<<"Welcome to the global scoreboard for the internatinal AXT test database!" << endl;
+  const int dbs = 25;
+  int userinput; 
+  int countryinput;
+  int testscore;
+    while(userinput != 2){
+        cout<<"Please choose one of the one following options:" << endl;
+        cout<<"1.Database"<<endl;
+        cout<<"2.Exit"<<endl;
+        cin>>userinput;
+        cout<<endl;
+
+        if(userinput == 1){
+            cout<<"Please choose one of the following countries to find the test score data:"<<endl;
+            cout<<"1.United States of America"<<endl;
+            cout<<"2.Australia"<<endl;
+            cout<<"3.Japan"<<endl;
+            cout<<"5.Russia"<<endl;
+            cin>>countryinput;
+            switch(countryinput){
+                case 1:
+                    cout<<"Please enter the test score you would like to search for (1300-1400) in the USA leaderboard: "<<endl;
+                    cin>>testscore;
+                    cout<<"With the rank of #" << abs(interpolation_search(usadataset,testscore,dbs)-25)<< " " <<usadataset.getName(interpolation_search(usadataset,testscore,dbs)) << " has the score " <<testscore<<endl;
+                    break;
+                case 2:
+                    cout<<"Please enter the test score you would like to search for (1300-1400) in the Australian leaderboard: "<<endl;
+                    cin>>testscore;
+                    interpolation_search(ausdataset,testscore,dbs);
+                    break;
+                case 3:
+                    cout<<"Please enter the test score you would like to search for (1300-1400) in the Japanese leaderboard: "<<endl;
+                    cin>>testscore;
+                    interpolation_search(jpndataset,testscore,dbs);
+                    break;
+                case 4:
+                    cout<<"Please enter the test score you would like to search for (1300-1400) in the Russian leaderboard: "<<endl;
+                    cin>>testscore;
+                    interpolation_search(rusdataset,testscore,dbs);
+                    break;
+                default:
+                    cout<<"Invalid input. Returning back to the main menu."<<endl;
+                    break;
+            }
+        }
+        if(userinput == 2){
+            cout<<"Exited." << endl;
+        }
+    }
 }
