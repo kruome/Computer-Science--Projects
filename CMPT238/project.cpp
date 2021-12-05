244b7c9344f9f482e42db01e1541b6d3794f2319
kruome <71830435+kruome@users.noreply.github.com>
2021-12-05 00:55:06 -0500

    Update project.cpp

--- CMPT238/project.cpp
+++ CMPT238/project.cpp
@@ -331,3 +331,341 @@ main ()
         }
     }
 }
+---------------------------------------------------------------------------------
+	#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <fstream>
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+  int score;
+  string name;
+  Node *prev, *next;
+
+  static Node *getnode (int score, string name)
+  {
+    Node *newNode = (Node *) malloc (sizeof (Node));
+      newNode->score = score;
+      newNode->name = name;
+      newNode->prev = newNode->next = NULL;
+      return newNode;
+  }
+};
+
+class Deque
+{
+  Node *front;
+  Node *rear;
+  int Size;
+
+public:
+    Deque ()
+  {
+    front = rear = NULL;
+    Size = 0;
+  }
+
+  void insertFront (int, string);
+  void insertRear (int, string);
+  void deleteFront ();
+  void deleteRear ();
+  int getFrontscore ();
+  int getRearscore ();
+  string getFrontcountry ();
+  string getFrontname ();
+  string getRearschool ();
+  string getRearcountry ();
+  string getRearname ();
+  int size ();
+  bool isEmpty ();
+  void erase ();
+  int printindex (int);
+
+};
+
+int
+Deque::printindex (int x)
+{
+  int val = -1;
+  Node *temp = new Node;
+  temp = front;
+  for (int i = 0; i < x; i++)
+    {
+      temp = temp->next;
+    }
+  val = temp->score;
+  return val;
+}
+
+bool Deque::isEmpty ()
+{
+  return (front == NULL);
+}
+
+int
+Deque::size ()
+{
+  return Size;
+}
+
+void
+Deque::insertFront (int score, string name)
+{
+  Node *newNode = Node::getnode (score, name);
+  if (newNode == NULL)
+    cout << "OverFlow\n";
+  else
+    {
+      if (front == NULL)
+	rear = front = newNode;
+
+      else
+	{
+	  newNode->next = front;
+	  front->prev = newNode;
+	  front = newNode;
+	}
+
+      Size++;
+    }
+}
+
+void
+Deque::insertRear (int score, string name)
+{
+  Node *newNode = Node::getnode (score, name);
+  if (newNode == NULL)
+    cout << "OverFlow\n";
+  else
+    {
+      if (rear == NULL)
+	front = rear = newNode;
+
+      else
+	{
+	  newNode->prev = rear;
+	  rear->next = newNode;
+	  rear = newNode;
+	}
+
+      Size++;
+    }
+}
+
+void
+Deque::deleteFront ()
+{
+  if (isEmpty ())
+    cout << "UnderFlow\n";
+
+  else
+    {
+      Node *temp = front;
+      front = front->next;
+
+      if (front == NULL)
+	rear = NULL;
+      else
+	front->prev = NULL;
+      free (temp);
+
+      Size--;
+    }
+}
+
+void
+Deque::deleteRear ()
+{
+  if (isEmpty ())
+    cout << "UnderFlow\n";
+
+  else
+    {
+      Node *temp = rear;
+      rear = rear->prev;
+
+      if (rear == NULL)
+	front = NULL;
+      else
+	rear->next = NULL;
+      free (temp);
+
+      Size--;
+    }
+}
+
+int
+Deque::getFrontscore ()
+{
+  if (isEmpty ())
+    {
+      return -1;
+    }
+  else
+    {
+      return front->score;
+    }
+}
+
+string Deque::getFrontname ()
+{
+  if (isEmpty ())
+    {
+      return "Empty.";
+    }
+  else
+    {
+      return front->name;
+    }
+}
+
+int
+Deque::getRearscore ()
+{
+  if (isEmpty ())
+    {
+      return -1;
+    }
+  else
+    {
+      return rear->score;
+    }
+}
+
+string Deque::getRearname ()
+{
+  if (isEmpty ())
+    {
+      return "Empty.";
+    }
+  else
+    {
+      return rear->name;
+    }
+}
+
+void
+Deque::erase ()
+{
+  rear = NULL;
+  while (front != NULL)
+    {
+      Node *temp = front;
+      front = front->next;
+      free (temp);
+    }
+  Size = 0;
+}
+
+int
+interpolation_search (Deque p, int value, int size)
+{
+  int low = 0;
+  int high = size - 1;
+  int mid = -1;
+
+  int l = p.getFrontscore ();
+  int h = p.getRearscore ();
+
+  while (low <= high && value >= p.printindex (low)
+	 && value <= p.printindex (high))
+    {
+      if (low == high)
+	{
+	  if (p.printindex (low) == value)
+	    {
+	      return low;
+	    }
+	  else
+	    {
+	      return -1;
+	    }
+	}
+      else
+	{
+	  mid =
+	    low +
+	    (((high - low) / (p.printindex (high) -
+			      p.printindex (low))) * (value -
+						      p.printindex (low)));
+	  if (p.printindex (mid) < value)
+	    {
+	      low = mid + 1;
+	    }
+	  else if (p.printindex (mid) > value)
+	    {
+	      high = mid - 1;
+	    }
+	  else if (p.printindex (mid) == value)
+	    {
+	      return mid;
+	    }
+	}
+    }
+  return -1;
+}
+
+int
+main ()
+{
+    
+  Deque usadataset;
+  
+  ifstream usadb;
+  usadb.open ("usa");
+  string usafile;
+  string usastrscore;
+  string usaname;
+  if (usadb.is_open ())
+    {
+      while (usadb)
+	{			// equivalent to myfile.good()
+	  getline (usadb, usafile);
+	  int usafilelength = usafile.length ();
+	  usastrscore = usafile.substr (0, 4);
+	  usaname = usafile.substr (5,usafilelength);
+	  int usaintscore = stoi(usastrscore);
+	  usadataset.insertRear(usaintscore, usaname);
+	  cout << usaintscore << " ";
+	  cout << usaname << '\n';
+	  if (usadb.eof())
+				break;
+	}
+    }
+  else
+    {
+      cout << "Couldn't open file\n";
+    }
+    
+  Deque ausdataset;
+  
+  ifstream ausdb;
+  ausdb.open ("australia");
+  string ausfile;
+  string ausstrscore;
+  string ausname;
+  if (ausdb.is_open ())
+    {
+      while (ausdb)
+	{			// equivalent to myfile.good()
+	  getline (ausdb, ausfile);
+	  int ausfilelength = ausfile.length ();
+	  ausstrscore = ausfile.substr (0, 4);
+	  ausname = ausfile.substr (5,ausfilelength);
+	  int ausintscore = stoi(ausstrscore);
+	  ausdataset.insertRear(ausintscore, ausname);
+	  cout << ausintscore << " ";
+	  cout << ausname << '\n';
+	  if (ausdb.eof())
+				break;
+	}
+    }
+  else
+    {
+      cout << "Couldn't open file\n";
+    }
+    
+   
+}
