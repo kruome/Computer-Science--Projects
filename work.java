ef081c127d606137c8cd8e6b8b6efaa47779e029
kruome <71830435+kruome@users.noreply.github.com>
2021-12-08 10:42:12 -0500

    Update work.java

--- work.java
+++ work.java
@@ -29,9 +29,12 @@ import javafx.scene.text.Text;
 
 public class JavaApplication1 extends Application{
     Scene mainScreen;
-    Scene option1Layout;
+    Scene option1;
+    Scene option2;
+    
     StackPane mainLayout;
     StackPane option1layout;
+    StackPane option2layout;
     Button button1;
     Button button2;
     Button button3;
@@ -41,7 +44,13 @@ public class JavaApplication1 extends Application{
     Label option1InstructorName;
     Label option1InstructorDepartment;
     Label option1InstructorLocation;
+    Label option2InstructorName;
+    Label option2InstructorDepartment;
+    Label option2InstructorLocation;
     TextField instructorIDField;
+    TextField ID2;
+    TextField Dept2;
+    TextField Location2;
     
     Label error1;
   public static void main(String[] args){
@@ -69,10 +78,11 @@ public class JavaApplication1 extends Application{
       returnButton.setText("Go back to main menu");
       
       submitButtonOption1 = new Button();
-      submitButtonOption1.setText("Ok");
+      submitButtonOption1.setText("Submit");
       
       mainLayout = new StackPane();
       option1layout = new StackPane();
+      option2layout = new StackPane();
       
       button1.setTranslateX(0);
       button1.setTranslateY(-50);
@@ -89,12 +99,25 @@ public class JavaApplication1 extends Application{
       option1label.setTranslateX(0);
       option1label.setTranslateY(-50);
       
+      ID2 = new TextField();
+      ID2.setPrefWidth(80);
+      ID2.setMaxWidth(80);
+      
+      Dept2 = new TextField();
+      Dept2.setPrefWidth(80);
+      Dept2.setMaxWidth(80);
+      
+      Location2 = new TextField();
+      Location2.setPrefWidth(80);
+      Location2.setMaxWidth(80);
+      
       instructorIDField = new TextField();
       instructorIDField.setPrefWidth(80);
       instructorIDField.setMaxWidth(80);
       
-      Scene mainScreen = new Scene (mainLayout,600,300);
-      Scene optionScreen = new Scene(option1layout,600,300);
+      mainScreen = new Scene (mainLayout,600,300);
+      option1 = new Scene(option1layout,600,300);
+      option2 = new Scene(option2layout,600,300);
       primaryStage.setScene(mainScreen);
       primaryStage.show();
       mainLayout.getChildren().add(button1);
@@ -102,115 +125,125 @@ public class JavaApplication1 extends Application{
       mainLayout.getChildren().add(button3);
       
       button1.setOnAction((ActionEvent event) -> {
-          primaryStage.setScene(optionScreen);
+          primaryStage.setScene(option1);
           option1layout.getChildren().add(returnButton);
           option1layout.getChildren().add(submitButtonOption1);
           option1layout.getChildren().add(instructorIDField);
           option1layout.getChildren().add(option1label);
     });
       button2.setOnAction((ActionEvent event) -> {
-          primaryStage.setScene(optionScreen);
+          primaryStage.setScene(option1);
+          option2layout.getChildren().add(ID2);
+          option1layout.getChildren().add(Dept2);
+          option1layout.getChildren().add(Location2);
           option1layout.getChildren().add(returnButton);
     });
+      //submitButtonOption2(){
+       // String strID = ID2.getText();
+       // String stdDept = Dept2.getText();
+       // String strLocation = Location2.getText();
+
+  //}
       button3.setOnAction((ActionEvent event) -> {
           System.exit(0);
     });
       returnButton.setOnAction((ActionEvent event) -> {
           option1layout.getChildren().clear();
+          option2layout.getChildren().clear();
+          clearTextField();
           primaryStage.setScene(mainScreen);
           primaryStage.show();
     });
-      submitButtonOption1.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            String strID = instructorIDField.getText();
-            if(strID.matches("\\d{4}")){
-                try {
-                    File instructorFile = new File("C:\\Users\\library.guest\\Downloads\\instructor.txt");
-                    Scanner instructorDatabase = new Scanner(instructorFile);
-                    File instructorFileSize = new File("C:\\Users\\library.guest\\Downloads\\instructor.txt");
-                    Scanner instructorDatabaseSize = new Scanner(instructorFileSize);
-                    
-                    File departmentFile = new File("C:\\Users\\library.guest\\Downloads\\department.txt");
-                    Scanner departmentDatabase = new Scanner(departmentFile);
-                    File departmentFileSize = new File("C:\\Users\\library.guest\\Downloads\\epartment.txt");
-                    Scanner departmentDatabaseSize = new Scanner(departmentFile);
-                    
-                    int ID = Integer.parseInt(strID);
-                    
-                    int arrOfDeptSize = 0;
-                    while (departmentDatabaseSize.hasNextLine()) {
-                        arrOfDeptSize++;
-                        departmentDatabaseSize.nextLine();
-                    }
-                    for (int j = 0; j < arrOfDeptSize; j++) {
-                        String line = departmentDatabase.nextLine();
-                        String[] deptField = line.split(",");
-                        departmentName.add(deptField[0]);
-                        departmentLocation.add(deptField[1]);
-                        departmentFund.add(Integer.parseInt(deptField[2].trim()));
-                    }
-                    
-                    ArrayList<Integer> instructorID = new ArrayList<>();
-                    ArrayList<String> instructorName = new ArrayList<>();
-                    ArrayList<String> instructorDepartment = new ArrayList<>();
-                    ArrayList<String> instructorLocation = new ArrayList<>();
-                    
-                    int arrOfInstSize = 0;
-                    while (instructorDatabaseSize.hasNextLine()) {
-                        arrOfInstSize++;
-                        instructorDatabaseSize.nextLine();
-                    }
-                    for (int j = 0; j < arrOfInstSize; j++) {
-                        String line = instructorDatabase.nextLine();
-                        String[] instrucField = line.split(",");
-                        instructorID.add(Integer.parseInt(instrucField[0].trim()));
-                        instructorName.add(instrucField[1]);
-                        instructorDepartment.add(instrucField[2]);
-                        for (int a = 0; a < arrOfDeptSize; a++) {
-                            if (instructorDepartment.get(j).equals(departmentName.get(a))) {
-                                instructorLocation.add(j, departmentLocation.get(a));
-                            }
-                        }
-                    }
-                    boolean option1flag = false;
-                    for(int i = 0; i < arrOfInstSize;i++){
-                        if(instructorID.get(i) == ID){
-                            option1InstructorName = new Label("Instructor Name: " + instructorName.get(i));
-                            option1InstructorDepartment = new Label("Instructor Department: " + instructorDepartment.get(i));
-                            option1InstructorLocation = new Label("Department Location: " + instructorLocation.get(i));
-                            option1InstructorName.setTranslateX(0);
-                            option1InstructorName.setTranslateY(-50);
-                            option1InstructorDepartment.setTranslateX(0);
-                            option1InstructorDepartment.setTranslateY(-25);
-                            option1InstructorLocation.setTranslateX(0);
-                            option1InstructorLocation.setTranslateY(0);
-                            removeButtons();
-                            option1layout.getChildren().add(option1InstructorName);
-                            option1layout.getChildren().add(option1InstructorDepartment);
-                            option1layout.getChildren().add(option1InstructorLocation);
-                            option1flag = true;
-                            clearTextField();
-                        }
-                    }
-                    if(option1flag == false){
-                        removeButtons();
-                        error1 = new Label("The ID does not exist in the file. Please try another ID.");
-                        option1layout.getChildren().add(error1);
-                        clearTextField();
-                        
-                    }
-                    
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }else{
-                error1 = new Label("Invalid input. Please press the go back button and try again.");
-                option1layout.getChildren().add(error1);
-                removeButtons();
-                clearTextField();
-            }
-        }
+      submitButtonOption1.setOnAction((ActionEvent event) -> {
+          String strID = instructorIDField.getText();
+          if(strID == null){
+              error1 = new Label("Invalid input. Please press the go back button and try again.");
+              option1layout.getChildren().add(error1);
+              removeButtons();
+          }else{
+          if(strID.matches("\\d{4}")){
+              try {
+                  File instructorFile = new File("C:\\Users\\library.guest\\Downloads\\instructor.txt");
+                  Scanner instructorDatabase = new Scanner(instructorFile);
+                  File instructorFileSize = new File("C:\\Users\\library.guest\\Downloads\\instructor.txt");
+                  Scanner instructorDatabaseSize = new Scanner(instructorFileSize);
+                  
+                  File departmentFile = new File("C:\\Users\\library.guest\\Downloads\\department.txt");
+                  Scanner departmentDatabase = new Scanner(departmentFile);
+                  File departmentFileSize = new File("C:\\Users\\library.guest\\Downloads\\epartment.txt");
+                  Scanner departmentDatabaseSize = new Scanner(departmentFile);
+                  
+                  int ID = Integer.parseInt(strID);
+                  
+                  int arrOfDeptSize = 0;
+                  while (departmentDatabaseSize.hasNextLine()) {
+                      arrOfDeptSize++;
+                      departmentDatabaseSize.nextLine();
+                  }
+                  for (int j = 0; j < arrOfDeptSize; j++) {
+                      String line = departmentDatabase.nextLine();
+                      String[] deptField = line.split(",");
+                      departmentName.add(deptField[0]);
+                      departmentLocation.add(deptField[1]);
+                      departmentFund.add(Integer.parseInt(deptField[2].trim()));
+                  }
+                  
+                  ArrayList<Integer> instructorID = new ArrayList<>();
+                  ArrayList<String> instructorName = new ArrayList<>();
+                  ArrayList<String> instructorDepartment = new ArrayList<>();
+                  ArrayList<String> instructorLocation = new ArrayList<>();
+                  
+                  int arrOfInstSize = 0;
+                  while (instructorDatabaseSize.hasNextLine()) {
+                      arrOfInstSize++;
+                      instructorDatabaseSize.nextLine();
+                  }
+                  for (int j = 0; j < arrOfInstSize; j++) {
+                      String line = instructorDatabase.nextLine();
+                      String[] instrucField = line.split(",");
+                      instructorID.add(Integer.parseInt(instrucField[0].trim()));
+                      instructorName.add(instrucField[1]);
+                      instructorDepartment.add(instrucField[2]);
+                      for (int a = 0; a < arrOfDeptSize; a++) {
+                          if (instructorDepartment.get(j).equals(departmentName.get(a))) {
+                              instructorLocation.add(j, departmentLocation.get(a));
+                          }
+                      }
+                  }
+                  boolean option1flag = false;
+                  for(int i = 0; i < arrOfInstSize;i++){
+                      if(instructorID.get(i) == ID){
+                          option1InstructorName = new Label("Instructor Name: " + instructorName.get(i));
+                          option1InstructorDepartment = new Label("Instructor Department: " + instructorDepartment.get(i));
+                          option1InstructorLocation = new Label("Department Location: " + instructorLocation.get(i));
+                          option1InstructorName.setTranslateX(0);
+                          option1InstructorName.setTranslateY(-50);
+                          option1InstructorDepartment.setTranslateX(0);
+                          option1InstructorDepartment.setTranslateY(-25);
+                          option1InstructorLocation.setTranslateX(0);
+                          option1InstructorLocation.setTranslateY(0);
+                          removeButtons();
+                          option1layout.getChildren().add(option1InstructorName);
+                          option1layout.getChildren().add(option1InstructorDepartment);
+                          option1layout.getChildren().add(option1InstructorLocation);
+                          option1flag = true;
+                      }
+                  }
+                  if(option1flag == false){
+                      removeButtons();
+                      error1 = new Label("The ID does not exist in the file. Please try another ID.");
+                      option1layout.getChildren().add(error1);                      
+                  }
+                  
+              } catch (FileNotFoundException ex) {
+                  Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+              }
+          }else{
+              error1 = new Label("Invalid input. Please press the go back button and try again.");
+              option1layout.getChildren().add(error1);
+              removeButtons();
+          }
+          }
     });
   }
   public void removeButtons(){
@@ -220,6 +253,12 @@ public class JavaApplication1 extends Application{
       }
   public void clearTextField(){
       instructorIDField.setText(null);
+      ID2.setText(null);
+      Dept2.setText(null);
+      Location2.setText(null);
+  }
+  public void option2Stage(){
+      
   }
 }
 /*
